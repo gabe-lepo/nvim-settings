4426abe3e444666c06f2e06e26265c24d8d5cb09
gabe-lepo <gabelepoutre@gmail.com>
2025-03-10 00:54:32 -0400

    make definitions open in new tabs

--- init.lua
+++ init.lua
@@ -54,10 +54,21 @@ function SetColorScheme(mode)
   end
 end
 
+-- Lock cursor function
+function ToggleScrollLock()
+  if vim.o.scrolloff == 999 then
+    vim.o.scrolloff = 0
+    print("Scroll lock disabled")
+  else
+    vim.o.scrolloff = 999
+    print("Scroll lock enabled")
+  end
+end
+
 -- Mason + mason-lspconfig setup
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = {"lua_ls", "pyright", "ts_ls", "zls"},
+  ensure_installed = {"lua_ls", "pyright", "ts_ls", "clangd"},
   automatic_installation = true,
 })
 
@@ -66,7 +77,13 @@ local telescope_builtin = require("telescope.builtin")
 local function on_attach(client, bufnr)
   local opts = { noremap = true, silent = false, buffer = bufnr }
 
-  vim.keymap.set("n", "gd", telescope_builtin.lsp_definitions, opts) -- Goto definitions
+  vim.keymap.set("n", "gd", function()
+    telescope_builtin.lsp_definitions({
+      jump_type = "tab",
+      reuse_win = false,
+    })
+  end, opts) -- Goto definitions in new buffer
+
   vim.keymap.set("n", "gr", telescope_builtin.lsp_references, opts) -- Find references
   vim.keymap.set("n", "gs", telescope_builtin.lsp_document_symbols, opts) -- Symbols in curr buff
   vim.keymap.set("n", "K", vim.lsp.buf.hover, opts) -- Symbol details hover
@@ -163,6 +180,7 @@ vim.api.nvim_create_user_command("Tree", "NERDTreeToggle", {})
 vim.api.nvim_create_user_command("Light", function() SetColorScheme("light") end, {})
 vim.api.nvim_create_user_command("Dark", function() SetColorScheme("dark") end, {})
 vim.api.nvim_create_user_command("Darker", function() SetColorScheme("darker") end, {})
+vim.api.nvim_create_user_command("Lock", function() ToggleScrollLock() end, {})
 
 -- Keybinds
 local map = vim.api.nvim_set_keymap
@@ -172,7 +190,7 @@ map("v", "<Leader>y", '"+y', opts)
 map("n", "<Leader>y", '"+yy', opts)
 map("n", "<Tab>", ":tabnext<CR>", opts)
 map("n", "<S-Tab>", ":tabprevious<CR>", opts)
-map("n", "<S-w>", ":tabclose<CR>", opts)
+map("n", "<S-w>", ":q<CR>", opts)
 map("n", "F", ":Telescope find_files<CR>", opts)
 map("n", "f", ":Telescope current_buffer_fuzzy_find<CR>", opts)
 map("n", "<Leader>f", ":Telescope live_grep<CR>", opts)
@@ -189,4 +207,4 @@ vim.o.softtabstop = 2			-- Num spaces Tab generates in insert mode
 vim.o.smarttab = true			-- Insert tabs based on current indent level
 
 vim.o.number = true				-- Line numbers
--- vim.o.scrolloff = 999	-- Keep cursor centered unless at top or bottom
+vim.o.scrolloff = 999     -- Keep cursor centered unless at top or bottom
